f584cb2c068373091e05e202939b6f036a26be32
RUDRA610-STACK <rchiku1998@gmail.com>
2025-01-25 12:10:29 +0530

    constructor

--- CucumberJava/src/test/java/pages/LoginPage.java
+++ CucumberJava/src/test/java/pages/LoginPage.java
@@ -11,6 +11,7 @@ public class LoginPage {
 	WebDriver driver;
 	public LoginPage(WebDriver driver) {
 		this.driver=driver;
+		//constructor declaration
 	}
 	public void enter_Username(String username) {
 		driver.findElement(txt_username).sendKeys(username);
